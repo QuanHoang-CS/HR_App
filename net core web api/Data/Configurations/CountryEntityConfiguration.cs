cdb6fe6496e9c4b100831f2cb543a7fdd63623f4
Quan <ellie.quanhoang136@gmail.com>
2026-05-21 13:20:41 -0500

    Modyfied Data folder into Confugirations and Context, so that we can use Fluent API for mapping database ->model

---
+++ net core web api/Data/Configurations/CountryEntityConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using net_core_web_api.Models.Domain;
+
+namespace net_core_web_api.Data.Configurations
+{
+    public class CountryEntityConfiguration : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            /*
+             * 1. Cannot chain HasKey() with Property().HasColumnName()
+             * 2. Cannot indicate several properties inside a domain model in 1 call, have to do 1 by 1
+             * 3. HasKey() is different from Required()
+             * 4. HasKey() property is implicitly a Required() (a.k.a not null) property.
+             */
+            builder.ToTable("countries");
+            builder.HasKey(x => x.CountryId);
+            // builder.HasForeignKey(x => x.RegionId);
+
+            builder.Property(x => x.CountryId)
+                .HasColumnName("country_id")
+                .HasMaxLength(2);
+
+            builder.Property(x => x.CountryName)
+                .HasColumnName("country_name")
+                .HasMaxLength(40);
+
+            builder.Property(x => x.RegionId)
+                .HasColumnName("region_id");
+        }
+    }
+}
---
+++ net core web api/Data/Context/HRDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using net_core_web_api.Models.Domain;
+
+namespace net_core_web_api.Data.Context
+{
+    public class HRDbContext : DbContext
+    {
+        public HRDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HRDbContext).Assembly);
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        /*
+        public DbSet<Dependants> Dependants { get; set; }
+        public DbSet<Employees> Employees{ get; set; }
+        public DbSet<Jobs> Jobs { get; set; }
+        public DbSet<Locations> Locations { get; set; }
+        public DbSet<Regions> Regions { get; set; }
+        */
+    }
+}
