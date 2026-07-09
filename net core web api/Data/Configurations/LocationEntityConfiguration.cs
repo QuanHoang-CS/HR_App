5540c3a189d2ba9b701fdcc2f12a68de5eaed70b
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-09 15:16:36 -0500

    Changed Locations.cs to Location.cs, implemented Location data model and LocationEntityConfiguration

---
+++ net core web api/Data/Configurations/LocationEntityConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using net_core_web_api.Models.Domain;
+
+
+namespace net_core_web_api.Data.Configurations
+{
+    public class LocationEntityConfiguration : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(EntityTypeBuilder<Location> builder)
+        {
+            builder.ToTable("locations");
+            builder.HasKey("LocationId");
+
+            builder.Property("LocationId")
+                .HasColumnName("location_id")
+                .IsRequired();
+
+            builder.Property("StreetAdDress")
+                .HasColumnName("street_address")
+                .HasMaxLength(40);
+
+            builder.Property("PostalCode")
+                .HasColumnName("postal_code")
+                .HasMaxLength(12);
+
+            builder.Property("City")
+                .HasMaxLength(30)
+                .IsRequired();
+
+            builder.Property("StateProvince")
+                .HasColumnName("state_province")
+                .HasMaxLength(25);
+
+            builder.Property("CountryId")
+                .HasColumnName("country_id")
+                .HasMaxLength(2)
+                .IsRequired();
+
+
+        }
+    }
+}
---
+++ net core web api/Models/Domain/Location.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace net_core_web_api.Models.Domain
+{
+    public class Location
+    {
+        public int LocationId { get; set; }
+        public string StreetAddress { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+        public string CountryId { get; set; }
+    }
+}
