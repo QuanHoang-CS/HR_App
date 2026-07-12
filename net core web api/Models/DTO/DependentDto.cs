8258ff984760c6a11e71f9827f17f128528a7f1d
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-12 15:32:38 -0500

    Implemented DependentDto

---
+++ net core web api/Models/DTO/DependentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace net_core_web_api.Models.DTO
+{
+    public class DependentDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Relationship { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
