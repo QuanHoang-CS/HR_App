9933894e46c3352b3436681b4dbb0a3bc18cef99
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-13 18:03:09 -0500

    Change Properties' type to nullable tp allow null value

--- net core web api/Models/DTO/DepartmentDto.cs
+++ net core web api/Models/DTO/DepartmentDto.cs
@@ -4,6 +4,6 @@
     {
         public int DepartmentId { get; set; }
         public string DepartmentName { get; set; }
-        public int LocationId { get; set; }
+        public int? LocationId { get; set; }
     }
 }
