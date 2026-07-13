5555bf94313fe0bc44a6922b9f4198a2ad4f667c
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-13 18:04:42 -0500

    Implemented GetAll

---
+++ net core web api/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+using net_core_web_api.Data.Context;
+using net_core_web_api.Models.DTO;
+
+namespace net_core_web_api.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private HRDbContext _dbContext;
+
+        public EmployeeController(HRDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var employees = await _dbContext.Employees.ToListAsync();
+            var employeesDto = new List<EmployeeDto>();
+            
+            foreach (var employee in employees)
+            {
+                employeesDto.Add(new EmployeeDto
+                {
+                    EmployeeId = employee.EmployeeId,
+                    FirstName = employee.FirstName,
+                    LastName = employee.LastName,
+                    Email = employee.Email,
+                    PhoneNumber = employee.PhoneNumber,
+                    HireDate = employee.HireDate,
+                    JobId = employee.JobId,
+                    Salary = employee.Salary,
+                    ManagerId = employee.ManagerId,
+                    DepartmentId = employee.DepartmentId,
+                });
+            }
+            return Ok(employeesDto);
+        }
+
+
+    }
+}
