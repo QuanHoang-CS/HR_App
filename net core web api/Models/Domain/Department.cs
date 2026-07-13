a3b1fa9e8122efe77093df48f39fb50e061c1d2e
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-13 18:00:08 -0500

    Change LocationId to nullable type to allow null value

--- net core web api/Models/Domain/Department.cs
+++ net core web api/Models/Domain/Department.cs
@@ -7,6 +7,6 @@ namespace net_core_web_api.Models.Domain
         [Key]
         public int DepartmentId { get; set; }
         public string DepartmentName { get; set; }
-        public int LocationId { get; set; }
+        public int? LocationId { get; set; }
     }
 }
