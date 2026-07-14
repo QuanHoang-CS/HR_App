4247b1f2bd07f59a0153f3a8a9a86c3d358b252c
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-13 19:06:26 -0500

    Get rid of Id Property

--- net core web api/Models/DTO/UpdateEmployeeRequestDto.cs
+++ net core web api/Models/DTO/UpdateEmployeeRequestDto.cs
@@ -2,7 +2,6 @@
 {
     public class UpdateEmployeeRequestDto
     {
-        public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
