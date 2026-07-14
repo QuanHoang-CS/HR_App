4b0f5203cdeea8035fa1805bcc1a3d5ebebb7a8a
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-13 19:14:57 -0500

    Change properties' type to nullable to allow null value

--- net core web api/Models/Domain/Job.cs
+++ net core web api/Models/Domain/Job.cs
@@ -4,7 +4,7 @@
     {
         public int JobId { get; set; }
         public string JobTitle { get; set; }
-        public decimal MinSalary { get; set; }
-        public decimal MaxSalary { get; set; }
+        public decimal? MinSalary { get; set; }
+        public decimal? MaxSalary { get; set; }
     }
 }
