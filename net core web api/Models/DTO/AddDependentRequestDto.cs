cf3bcc2fa241349630ef28fac6f5299c49529a74
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-15 14:29:59 -0500

    Implemented AddDependentRequestDto

---
+++ net core web api/Models/DTO/AddDependentRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace net_core_web_api.Models.DTO
+{
+    public class AddDependentRequestDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Relationship { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
