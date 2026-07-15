7dbd17b9d5d8bdeef20a0a38de89a6e4e800eced
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-15 14:55:36 -0500

    Implemented GetAll

---
+++ net core web api/Controllers/LocationController.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using net_core_web_api.Data;
+using net_core_web_api.Data.Context;
+using net_core_web_api.Models;
+
+namespace net_core_web_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class LocationController : ControllerBase
+    {
+        private HRDbContext _dbContext;
+
+        public LocationController(HRDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var locations = await _dbContext.Locations.ToListAsync();
+
+            return Ok(locations);
+        }
+    }
+}
