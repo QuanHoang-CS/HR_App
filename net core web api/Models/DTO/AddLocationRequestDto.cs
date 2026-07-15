ffacfe755008a144eb0d4128d2b36144bfb1d3ba
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-15 14:47:03 -0500

    Implemented AddLocationRequestDto and locationDto

---
+++ net core web api/Models/DTO/AddLocationRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace net_core_web_api.Models.DTO
+{
+    public class AddLocationRequestDto
+    {
+        public string? StreetAddress { get; set; }
+        public string? PostalCode { get; set; }
+        public string City { get; set; }
+        public string? StateProvince { get; set; }
+        public string CountryId { get; set; }
+    }
+}
---
+++ net core web api/Models/DTO/LocationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace net_core_web_api.Models.DTO
+{
+    public class LocationDto
+    {
+        public int LocationId { get; set; }
+        public string? StreetAddress { get; set; }
+        public string? PostalCode { get; set; }
+        public string City { get; set; }
+        public string? StateProvince { get; set; }
+        public string CountryId { get; set; }
+    }
+}
