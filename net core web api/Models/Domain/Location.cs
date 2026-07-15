89d48746cc7296ca2975202cf7534ae72c4eb40b
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-15 14:47:46 -0500

    Change some properties' type to nullable to allow null value.

--- net core web api/Models/Domain/Location.cs
+++ net core web api/Models/Domain/Location.cs
@@ -5,10 +5,10 @@ namespace net_core_web_api.Models.Domain
     public class Location
     {
         public int LocationId { get; set; }
-        public string StreetAddress { get; set; }
-        public string PostalCode { get; set; }
+        public string? StreetAddress { get; set; }
+        public string? PostalCode { get; set; }
         public string City { get; set; }
-        public string StateProvince { get; set; }
+        public string? StateProvince { get; set; }
         public string CountryId { get; set; }
     }
 }
