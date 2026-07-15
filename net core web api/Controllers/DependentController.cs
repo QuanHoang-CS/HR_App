b23df8ca76954f37bde2e836a196914a18bbf25e
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-07-15 13:59:57 -0500

    Implemented GetByDependentId and GetByEmployeeId

--- net core web api/Controllers/DependentController.cs
+++ net core web api/Controllers/DependentController.cs
@@ -43,5 +43,70 @@ namespace net_core_web_api.Controllers
             return Ok(dependentDtoList);
         }
 
+        [HttpGet("dependentId")]
+        public async Task<IActionResult> GetByDependentId([FromQuery] int id)
+        {
+            var dependent = await _dbContext.Dependents.FirstOrDefaultAsync(d => d.Id == id);
+
+            if(dependent == null)
+            {
+                return NotFound($"No dependent with id: \'{id}\'");
+            }
+
+            var dependentDto = new DependentDto
+            {
+                Id = dependent.Id,
+                FirstName = dependent.FirstName,
+                LastName = dependent.LastName,
+                Relationship = dependent.Relationship,
+                EmployeeId = dependent.EmployeeId,
+            };
+
+            return Ok(dependentDto);
+        }
+
+        [HttpGet("employeeId")]
+        public async Task<IActionResult> GetByEmployeeId(int id)
+        {
+            // Should I use async filter or synchronous filter here?
+            // Af I keep implementing, I gotta check if dependents yield any result, so I have to execute it with Any() or
+            // ToListAsync() anyway. So let's just do it now below
+            // var dependents = _dbContext.Dependents.Where(d => d.EmployeeId == id);
+            var dependents = await _dbContext.Dependents.Where(d => d.EmployeeId == id).ToListAsync();
+            var dependentDtoList = new List<DependentDto>();
+
+            if(dependents == null)
+            {
+                return NotFound($"Employee with id: \'{id}\' is either not found, or have no dependents");
+            }
+
+            foreach(var dependent in dependents)
+            {
+                dependentDtoList.Add(new DependentDto
+                {
+                    Id = dependent.Id,
+                    FirstName = dependent.FirstName,
+                    LastName = dependent.LastName,
+                    Relationship = dependent.Relationship,
+                    EmployeeId = dependent.EmployeeId,
+                });
+            }
+
+            return Ok(dependentDtoList);
+        }
+
+        [HttpDelete("dependentId")]
+        public async Task<IActionResult> deleteByDependentId([FromQuery] int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("EmployeeId")]
+        public async Task<IActionResult> deleteByEmployeeId([FromQuery] int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
