43fb52841590dfc6800f39599b008b2096080c8d
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-08-14 20:57:50 -0500

    Added seeder for Identity/Role database

---
+++ web_api/Data/Identity/IdentitySeeder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyApp.API.Data.Identity
+{
+    public static class IdentitySeeder
+    {
+        public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            string[] roles =
+            {
+                "Admin",
+                "HR",
+                "Manager",
+                "Employee"
+            };
+
+            foreach(var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                    await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+}
