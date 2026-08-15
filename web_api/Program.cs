cfdee2ef17e3c00d65f14c52075cf4c1ca55d511
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-08-14 21:00:11 -0500

    Modified Program.cs for Authentication and Seeding

--- web_api/Program.cs
+++ web_api/Program.cs
@@ -1,12 +1,18 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MyApp.API.Data.Context;
+using MyApp.API.Data.Identity;
+using MyApp.API.Models.Identity;
+using System.Text;
 
 namespace MyApp.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -24,15 +30,36 @@ namespace MyApp.API
                                                 // This error occurs when you have enabled the Swagger middleware in your application's request pipeline,
                                                 // but haven't registered the required services in the dependency injection (DI) container.
 
-                                                // This error occurs when you have enabled
-                                                // the Swagger middleware in your application's
-                                                // request pipeline, but haven't registered
-                                                // the required services in the dependency injection (DI) container. 
+            // This error occurs when you have enabled
+            // the Swagger middleware in your application's
+            // request pipeline, but haven't registered
+            // the required services in the dependency injection (DI) container. 
 
             // Add your db to the depecdency injection
-            var connectionString = builder.Configuration.GetConnectionString("HR");
-            builder.Services.AddDbContext<HRDbContext>(options => options.UseSqlServer(connectionString));
-            
+            var connectionStringToHR = builder.Configuration.GetConnectionString("HR");
+            var connectionStringToRole = builder.Configuration.GetConnectionString("Authentication");
+
+            if (connectionStringToHR == null || connectionStringToRole == null)
+                throw new InvalidOperationException("Connection string to HR or HR role not found");
+
+            builder.Services.AddDbContext<HRDbContext>(options => options.UseSqlServer(connectionStringToHR));
+            builder.Services.AddDbContext<ApplicationIdentityDbContext>(options => options.UseSqlServer(connectionStringToRole));
+
+            builder.Services.AddIdentityCore<ApplicationUser>()
+                            .AddRoles<IdentityRole>()
+                            .AddTokenProvider<DataProtectorTokenProvider<ApplicationUser>>("HRDb")
+                            .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+                            .AddDefaultTokenProviders();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 7;
+                options.Password.RequiredUniqueChars = 1;
+            });   
             /*
             builder.Services.AddSession(options =>
             {
@@ -41,18 +68,43 @@ namespace MyApp.API
 
             //builder.Configuration.
             */
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+
+                    //
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
-                app.UseSwagger();   // Add by me
-                app.UseSwaggerUI(); // Add by me, This enables the web UI
+                app.UseSwagger();       // Add by me
+                app.UseSwaggerUI();     // Add by me, This enables the web UI
+            }
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                await IdentitySeeder.SeedRolesAsync(roleManager);
             }
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();    // Authenticating b4 authorizing
+            
             app.UseAuthorization();
 
             app.MapControllers();
