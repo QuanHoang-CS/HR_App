52710adf718e76b901a8bd427077055a9b90bc24
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-08-16 20:04:56 -0500

    Added JwtService

---
+++ web_api/Services/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿using MyApp.API.Models.Identity;
+
+namespace MyApp.API.Services
+{
+    public interface IJwtService
+    {
+        Task<string> CreateTokenAsync(ApplicationUser user);
+    }
+}
---
+++ web_api/Services/JwtService.cs
@@ -0,0 +1,12 @@
+﻿using MyApp.API.Models.Identity;
+
+namespace MyApp.API.Services
+{
+    public class JwtService : IJwtService
+    {
+        public async Task<string> CreateTokenAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
