00e65f217735740d2465be54cbae06793f50eae2
Quan <54778442+QuanHoang-CS@users.noreply.github.com>
2026-08-16 20:04:31 -0500

    Added Login request and Register Request DTO

---
+++ web_api/Models/DTO/LoginRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyApp.API.Models.DTO
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ web_api/Models/DTO/RegisterRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyApp.API.Models.DTO
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        public string Username { get; set;  }
+        [Required]
+        public string Password { get; set; }
+        public string? Roles { get; set; }
+        [Required]
+        public int EmployeeId {  get; set; }
+
+    }
+}
